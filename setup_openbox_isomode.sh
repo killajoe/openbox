a5cfebb34147d9337b651d3efe4fa83668002501
Johannes Kamprad <archlinux@kamprad.net>
2025-04-01 13:16:44 +0200

    Update setup_openbox_isomode.sh

--- setup_openbox_isomode.sh
+++ setup_openbox_isomode.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 username="$1"
 
-git clone https://github.com/EndeavourOS-Community-Editions/openbox.git
+git clone https://github.com/killajoe/openbox.git
 cd openbox
 cp -R .config /home/$username/                                               
 cp .gtkrc-2.0 /home/$username/
@@ -12,6 +12,6 @@ chmod -R +x /home/$username/.config/openbox/scripts
 chmod -R +x /home/$username/.config/rofi/scripts
 cd ..
 rm -rf openbox
-wget https://raw.githubusercontent.com/EndeavourOS-Community-Editions/openbox/main/packages-repository.txt
+wget https://raw.githubusercontent.com/killajoe/openbox/main/packages-repository.txt
 pacman -S --needed --noconfirm - < packages-repository.txt
 systemctl enable lightdm
